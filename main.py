72d4710817b663d252b64b604d2cb2500e1c5622
Renan <104283869+Renan023@users.noreply.github.com>
2022-09-03 16:34:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ for x in range (0,x):#ciclo de repetição determinada em quantos ciclos de repe
     print('='*50)
     d['Nome'] = input('Nome: ')
     d['Ano de nascimento'] = int(input('Nascimento '))
-    id = at[1] - nasc
+    id = at[1] - d['Ano de nascimento']
     print('Idade {} anos'.format(id))
     d['Sexo'] = input('Sexo ').strip().upper()[0]
     while d['Sexo'] not in 'MmFf':#ciclo de repetição que  irá validar o sexo
         print('Digite novamente')
         d['Sexo'] = input('Sexo ').strip().upper()[0]
-    print('{}, {} anos, sexo {}'.format(n,id,s))
+    print(f'{d["Nome"]}, {id} anos, sexo {d["Sexo"]}')
     d['Valor'] = float(input('Sua compra ficou em R$ '))
     print('Processando....')
     time.sleep(0.5)
@@ -36,17 +36,17 @@ for x in range (0,x):#ciclo de repetição determinada em quantos ciclos de repe
 [2] 2 vezes\n[3] 3 vezes\n[4] 4 vezes\n[5] 5 vezes - OBS:. até R$ 1.500.00\n[6] 6 vezes
 [7] 7 vezes - OBS:. até R$ 3.000.00\n[8] 8 vezes\n[9] 9 vezes\n[10] 10 vezes - OBS:. de R$ 5.500.00 """)
     op = int(input('Qual opção ?'))
-    if op == 0:desconto(v, 90, 100)
-    elif op == 1:desconto(v, 95, 100)
-    elif op == 2:acrescimoparcelado(v, 100, 100, 2)
-    elif op == 3:acrescimoparcelado(v, 110, 100, 3)
-    elif op == 4:acrescimoparcelado( v , 115, 100, 4)
-    elif op == 5 and v >= 1500.00:acrescimoparcelado(v, 120, 100, 5)
-    elif op == 6:acrescimoparcelado(v, 125, 100, 6)
-    elif op == 7 and v >= 3000.00:acrescimoparcelado(v, 130, 100, 7)
-    elif op == 8:acrescimoparcelado(v, 135, 100, 8)
-    elif op == 9:acrescimoparcelado(v, 140, 100, 9)
-    elif op == 10 and v >= 5500.00:acrescimoparcelado(v, 145, 100, 10)
+    if op == 0:desconto(d['Valor'], 90, 100)
+    elif op == 1:desconto(d['Valor'], 95, 100)
+    elif op == 2:acrescimoparcelado(d['Valor'], 100, 100, 2)
+    elif op == 3:acrescimoparcelado(d['Valor'], 110, 100, 3)
+    elif op == 4:acrescimoparcelado( d['Valor'] , 115, 100, 4)
+    elif op == 5 and d['Valor'] >= 1500.00:acrescimoparcelado(d['Valor'], 120, 100, 5)
+    elif op == 6:acrescimoparcelado(d['Valor'], 125, 100, 6)
+    elif op == 7 and d['Valor'] >= 3000.00:acrescimoparcelado(d['Valor'], 130, 100, 7)
+    elif op == 8:acrescimoparcelado(d['Valor'], 135, 100, 8)
+    elif op == 9:acrescimoparcelado(d['Valor'], 140, 100, 9)
+    elif op == 10 and d['Valor'] >= 5500.00:acrescimoparcelado(d['Valor'], 145, 100, 10)
     else:
         print('Opção Inoperante')
         time.sleep(0.5)
