b6bf5cbab321a830fb38348400ea76233030e0c3
Renan <104283869+Renan023@users.noreply.github.com>
2022-09-03 16:08:37 -0300

    Add files via upload
    Foi implementado as def quanto aos descontos ou acréscimos, parcelados. As tuplas na variável at e dicionários para os  dados de cadastro serem copiados nas listas e mostrados no final

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import datetime, time
-hj, at = datetime.date.today(), datetime.date.today().year
-print(hj)
+def acrescimoparcelado(v, d, tot, par):
+    acresc= v*d/tot
+    pac = d - tot
+    xs = acresc/par
+    print(f'Valor parcelado em {par} vezes terá acréscimo de {pac}% fica em R$ {(float(xs)):.2f}')
+def desconto (v, d, total):
+    desc= v*d/total
+    pdesc = total -d # porcentagem do desconto
+    print(f'O valor R$ {(float(v)):.2f} com desconto de {pdesc}% é R$ {(float(desc)):.2f} ')
+
+
+at = (datetime.date.today(), datetime.date.today().year) #tupla com a importação datetime
+d = {}
+l = []
+#programa principal
+print(at[0])
 print('Me chamo Isadora. Em que posso ajudar?')
 x = int(input('Quantos cadastro de compra '))
-for x in range (0,x):
-    print('Processando....')
-    time.sleep(0.5)
+for x in range (0,x):#ciclo de repetição determinada em quantos ciclos de repetição o usuário quer realizar
     print('='*50)
-    n = input('Nome: ')
-    nasc = int(input('Nascimento '))
-    id = at - nasc
+    d['Nome'] = input('Nome: ')
+    d['Ano de nascimento'] = int(input('Nascimento '))
+    id = at[1] - nasc
     print('Idade {} anos'.format(id))
-    s = input('Sexo ').strip().upper()[0]
-    while s not in 'MmFf':
+    d['Sexo'] = input('Sexo ').strip().upper()[0]
+    while d['Sexo'] not in 'MmFf':#ciclo de repetição que  irá validar o sexo
         print('Digite novamente')
-        s = input('Sexo ').strip().upper()[0]
+        d['Sexo'] = input('Sexo ').strip().upper()[0]
     print('{}, {} anos, sexo {}'.format(n,id,s))
-    v = float(input('Sua compra ficou em R$ '))
+    d['Valor'] = float(input('Sua compra ficou em R$ '))
     print('Processando....')
     time.sleep(0.5)
     print('='*50)
@@ -24,54 +36,27 @@ for x in range (0,x):
 [2] 2 vezes\n[3] 3 vezes\n[4] 4 vezes\n[5] 5 vezes - OBS:. até R$ 1.500.00\n[6] 6 vezes
 [7] 7 vezes - OBS:. até R$ 3.000.00\n[8] 8 vezes\n[9] 9 vezes\n[10] 10 vezes - OBS:. de R$ 5.500.00 """)
     op = int(input('Qual opção ?'))
-    if op == 0:
-        d = v * 90 / 100
-        print('A vista no dinheiro ou débito o valor fica R$ {:.2f}'.format(d))
-    elif op == 1:
-        d = v * 95 / 100
-        print('A vista no cartão de crédito fica R$ {:.2f}'.format(d))
-    elif op == 2:
-        pa = v / 2
-        print('O valor parcelado não sofrerá acréscimo ficou R$ {:.2f}'.format(pa))
-    elif op == 3:
-        ac = v / 115 * 100
-        pa = ac / 3
-        print('O valor parcelado sofrerá um acréscimo de 15% e ficou R$ {:.2f} '.format(pa))
-    elif op == 4:
-        ac = v * 115 / 100
-        pa = ac / 4
-        print('O valor parcelado terá um acréscimo de 15% e ficará no valor de R$ {:.2f}'.format(pa))
-    elif op == 5 and v >= 1500.00:
-        ac = v * 120 / 100
-        pa = ac / 5
-        print('Valor parcelado em 5x com acréscimo de 20% no valor de R$ {:.2f}'.format(pa))
-    elif op == 6:
-        ac = v * 120 / 100
-        pa = ac / 6
-        print('Valor parcelado em 6x com acréscimo de 20% no valor de R$ {:.2f}'.format(pa))
-    elif op == 7 and v >= 3000.00:
-        ac = v * 125 / 100
-        pa = ac / 7
-        print('Valor parcelado em 7x com acréscimo de 25% no valor de R$ {:.2f}'.format(pa))
-    elif op == 8:
-        ac = v * 125 / 100
-        pa = ac / 8
-        print('Valor parcelado em 8x com acréscimo de 25% no valor de R$ {:.2f}'.format(pa))
-    elif op == 9:
-        ac = v * 130 / 100
-        pa = ac / 9
-        print('Valor parcelado em 9x com acréscimo de 30% no valor de R$ {:.2f}'.format(pa))
-    elif op == 10 and v >= 5500.00:
-        ac = v * 130 / 100
-        pa = ac / 10
-        print('Valor parcelado em 10x com acréscimo de 30% no valor de R$ {:.2f}'.format(pa))
+    if op == 0:desconto(v, 90, 100)
+    elif op == 1:desconto(v, 95, 100)
+    elif op == 2:acrescimoparcelado(v, 100, 100, 2)
+    elif op == 3:acrescimoparcelado(v, 110, 100, 3)
+    elif op == 4:acrescimoparcelado( v , 115, 100, 4)
+    elif op == 5 and v >= 1500.00:acrescimoparcelado(v, 120, 100, 5)
+    elif op == 6:acrescimoparcelado(v, 125, 100, 6)
+    elif op == 7 and v >= 3000.00:acrescimoparcelado(v, 130, 100, 7)
+    elif op == 8:acrescimoparcelado(v, 135, 100, 8)
+    elif op == 9:acrescimoparcelado(v, 140, 100, 9)
+    elif op == 10 and v >= 5500.00:acrescimoparcelado(v, 145, 100, 10)
     else:
         print('Opção Inoperante')
         time.sleep(0.5)
     print('Processando....')
 print('Obrigado pela sua visita em nossa loja. Tenha um ótimo dia s2')
-print('Aguardamos sua próxima visita a nosta loja ')
+print('Aguardamos sua próxima visita a nossa loja ')
 print('Próximo cadastro....')
 print('='*50)
 time.sleep(0.5)
+l.append(d.copy())
+d.clear()
+print(l)
 
